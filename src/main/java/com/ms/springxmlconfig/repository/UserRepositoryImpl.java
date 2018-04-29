bc0317fb8ce8505f32d4fe539dc1f652f8306fb3
Mandar.Shidhore <mandar.shidhore@travelport.com>
2018-04-29 16:29:09 -0600

    reading properties file using plain XML configuration and Java annotation configuration

--- src/main/java/com/ms/springxmlconfig/repository/UserRepositoryImpl.java
+++ src/main/java/com/ms/springxmlconfig/repository/UserRepositoryImpl.java
@@ -3,15 +3,30 @@ package com.ms.springxmlconfig.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.ms.springxmlconfig.model.User;
 
 public class UserRepositoryImpl implements UserRepository {
 
-	/* (non-Javadoc)
+	private String usernameInJavaClass;
+
+	@Value("${passwordInPropertiesFile}")
+	private String passwordInJavaClass;
+
+	public void setUsernameInJavaClass(String usernameInJavaClass) {
+		this.usernameInJavaClass = usernameInJavaClass;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.ms.springxmlconfig.repository.UserRepository#getUser()
 	 */
 	@Override
 	public List<User> getUser() {
+		System.out.println("user name in properties file = " + usernameInJavaClass);
+		System.out.println("password in properties file = " + passwordInJavaClass);
 		List<User> users = new ArrayList<>();
 		User user = new User();
 		user.setUserName("MS");
